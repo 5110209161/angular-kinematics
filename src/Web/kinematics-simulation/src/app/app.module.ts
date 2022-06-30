63085627c46ef7e3ed269d2ae9b7ca392768e713
5110209161 <hbin6358@163.com>
2022-06-30 17:14:55 +0800

    Introduce PriimeNG

--- src/Web/kinematics-simulation/src/app/app.module.ts
+++ src/Web/kinematics-simulation/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CollaRobotComponent } from './colla-robot/colla-robot.component';
+import { LayoutComponent } from './layout/layout.component';
+
+import { SliderModule } from 'primeng/slider';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CollaRobotComponent
+    CollaRobotComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    SliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
